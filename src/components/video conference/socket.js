aea77aa743126fa260137d83a8a2914990b5034d
Mohamed Osama <76261253+mohameddosamaa74@users.noreply.github.com>
2022-05-13 00:39:43 +0200

    Update socket.js

--- src/components/video conference/socket.js
+++ src/components/video conference/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 // const socket = io('http://localhost:3001');
-const socket = io('https://backend.connect-asl.site/');
+const socket = io('https://socket.connect-asl.site/');
 export default socket;
